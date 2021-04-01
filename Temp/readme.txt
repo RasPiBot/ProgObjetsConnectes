8390237dadfce97477f46a23b32a833c574dd4fa
TheSimonizer59 <simonlabbe59@gmail.com>
2021-04-01 12:23:39 -0400

    Ajout du script pour DHT11

---
+++ Temp/readme.txt
@@ -0,0 +1,15 @@
+Petit script pour avoir la temperature et l'humidité a partir d'un sensor DHT11
+
+Pour commancer, installer la librairie suivante :
+
+    pip3 install adafruit-circuitpython-dht
+
+lancer aussi cette commande :
+
+    sudo apt-get install libgpiod2
+
+Voici les Pin utiliser : 
+
++ = 3.3V
+- = Ground
+Out = GPIO 17
---
+++ Temp/temp.py
@@ -0,0 +1,21 @@
+import time
+import board
+import adafruit_dht
+
+#Initialisation du sensor de temperature, sur la pin GPIO 17
+dhtDevice = adafruit_dht.DHT11(board.D17, use_pulseio = False)
+
+#Boucle de lecture / écriture de la température/humidité
+while True:
+    try:
+         # Importations des données vers les variables
+         temperature_c = dhtDevice.temperature
+         temperature_f = temperature_c * (9 / 5) + 32
+         humidity = dhtDevice.humidity
+         print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+               .format(temperature_f, temperature_c, humidity))
+        # DHT11 pas souvent fiable, message d'erreur en cas de problème
+    except RuntimeError as error:     
+         print("Erreur, veuillez réessayer")
+         # Le DHT11 est pret au 2 secondes, définition d'un timer au 2 secondes.
+    time.sleep(2.0)
